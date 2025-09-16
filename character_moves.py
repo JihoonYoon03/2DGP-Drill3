3f50980f31539935626dcdd7e0fb8214fbbd0024
laptop-msi <wlgns7922@gmail.com>
2025-09-16 11:57:45 +0900

    사각운동 위치별 뼈대 추가

--- character_moves.py
+++ character_moves.py
@@ -4,7 +4,28 @@ open_canvas()
 boy = load_image('character.png')
 grass = load_image('grass.png')
 
+
+def move_bottom():
+    pass
+
+
+def move_up():
+    pass
+
+
+def move_top():
+    pass
+
+
+def move_down():
+    pass
+
+
 def move_rectangle():
+    move_bottom()
+    move_up()
+    move_top()
+    move_down()
     clear_canvas_now()
     boy.draw_now(400, 90)
     grass.draw_now(400, 30)
