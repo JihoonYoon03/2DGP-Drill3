1f8e0cb485e505ecedf1a4a06c6d7bf23fa7bd95
laptop-msi <wlgns7922@gmail.com>
2025-09-16 19:02:21 +0900

    삼각운동 완성

--- character_moves.py
+++ character_moves.py
@@ -8,30 +8,25 @@ grass = load_image('grass.png')
 def move_bottom_left():
     for x in range(20, 400, 5):
         draw_everything(x, 90)
-    pass
 
 def move_bottom_right():
     for x in range(400, 780, 5):
         draw_everything(x, 90)
-    pass
 
 
 def move_up():
     for y in range(90, 560, 5):
         draw_everything(780, y)
-    pass
 
 
 def move_top():
     for x in range(780, 20, -5):
         draw_everything(x, 560)
-    pass
 
 
 def move_down():
     for y in range(560, 90, -5):
         draw_everything(20, y)
-    pass
 
 
 def move_rectangle():
@@ -40,7 +35,6 @@ def move_rectangle():
     move_top()
     move_down()
     move_bottom_left()
-    pass
 
 
 def draw_everything(x: int, y: int):
@@ -51,27 +45,34 @@ def draw_everything(x: int, y: int):
 
 
 def move_tri_up():
-    delX = 5
-    delY = 560 / (380 / delX)
     x = 780
     y = 90
+    delX = 5
+    delY = 470 / (380 / delX)
+
     while x > 400 and y < 560:
         draw_everything(x, y)
         x -= delX
         y += delY
-    pass
 
 
 def move_tri_down():
-    pass
+    x = 400
+    y = 560
+    delX = 5
+    delY = 470 / (380 / delX)
+
+    while x > 20 and y > 90:
+        draw_everything(x, y)
+        x -= delX
+        y -= delY
 
 
 def move_triangle():
     move_bottom_right()
     move_tri_up()
     move_tri_down()
-    
-    pass
+    move_bottom_left()
 
 
 def move_circle():
